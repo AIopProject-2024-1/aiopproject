0264c7007ad324b18e867e971cce0601626d3d6b
hyejun <127915444+nojungsin@users.noreply.github.com>
2024-05-23 01:18:10 +0900

    navbar

---
+++ app/src/main/java/com/example/project2024_1/buttonlayout.java
@@ -0,0 +1,43 @@
+package com.example.project2024_1;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class buttonlayout extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.buttonlayout);
+
+        ImageButton btnhome = findViewById(R.id.btnhome);
+        ImageButton btnsetting = findViewById(R.id.btnsetting);
+        ImageButton btnmypage = findViewById(R.id.btnmypage);
+        btnhome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), home.class);
+                startActivity(intent);
+            }
+        });
+
+        btnsetting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), setting.class);
+                startActivity(intent);
+            }
+        });
+
+        btnmypage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), mypage.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
