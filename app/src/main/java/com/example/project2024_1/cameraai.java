b8e8464b7eb65414f3552abee160af04951e166f
hyejun <127915444+nojungsin@users.noreply.github.com>
2024-06-12 13:14:59 +0900

    cameraxmlbasic

--- app/src/main/java/com/example/project2024_1/cameraai.java
+++ app/src/main/java/com/example/project2024_1/cameraai.java
@@ -4,11 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.mlkit.vision.pose.PoseDetection;
+import com.google.mlkit.vision.pose.PoseDetector;
+import com.google.mlkit.vision.pose.defaults.PoseDetectorOptions;
+
 public class cameraai extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cameraai);
+
+        PoseDetectorOptions options =
+                new PoseDetectorOptions.Builder()
+                        .setDetectorMode(PoseDetectorOptions.STREAM_MODE)
+                        .build();
+
+        PoseDetector poseDetector = PoseDetection.getClient(options);
     }
 }
