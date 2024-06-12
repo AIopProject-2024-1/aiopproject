6c18e2dc5a28a09599c59ce2475051697550ec69
hyejun <127915444+nojungsin@users.noreply.github.com>
2024-06-13 00:24:57 +0900

    camerai

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,3 +3,10 @@ plugins {
     id("com.android.application") version "8.4.0" apply false
     id("com.google.gms.google-services") version "4.4.1" apply false
 }
+
+buildscript{
+    dependencies{
+        classpath("com.android.tools.build:gradle:8.4.2")
+        classpath("com.google.gms:google-services:4.4.2")
+    }
+}
