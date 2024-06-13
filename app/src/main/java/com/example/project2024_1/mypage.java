c19ebd8231e48952f3bb689212ca64c6891e3744
hyejun <127915444+nojungsin@users.noreply.github.com>
2024-06-14 07:56:30 +0900

    camerai

--- app/src/main/java/com/example/project2024_1/mypage.java
+++ app/src/main/java/com/example/project2024_1/mypage.java
@@ -1,9 +1,12 @@
 package com.example.project2024_1;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,5 +21,33 @@ public class mypage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mypage);
+
+        ImageButton btnhome = findViewById(R.id.btnhome);
+        ImageButton btnsetting = findViewById(R.id.btnsetting);
+        ImageButton btnmypage = findViewById(R.id.btnmypage);
+        btnhome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), home.class);
+                startActivity(intent);
+            }
+        });
+
+        btnsetting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), setting.class);
+                startActivity(intent);
+            }
+        });
+
+        btnmypage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), mypage.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
